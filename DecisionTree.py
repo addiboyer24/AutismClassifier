360c199b6be63b87528243c795608810e84a802d
addiboyer24 <Addison.Boyer@UMontana.edu>
2020-04-29 02:21:54 -0600

    Adding cohesive notebook showcasing work done on project

--- DecisionTree.py
+++ DecisionTree.py
@@ -5,6 +5,7 @@ import matplotlib.pyplot as plt
 import sys
 from graphviz import Digraph
 from collections import defaultdict
+from prettytable import PrettyTable
 sys.setrecursionlimit(10000)
 
 
@@ -136,7 +137,9 @@ def get_data_partitions(data, features=None):
     partitions = defaultdict(list)
     for idx in range(0, data.shape[1]-1):
         # one of selected features?
+        #print(features)
         if(idx in features):
+            
             unique_scores = np.unique(data[:, idx])
             '''
             if(len(unique_scores) == 1): # edge case what if unique score == 1
@@ -429,7 +432,9 @@ def get_sensitivity(confusion_matrix):
     return confusion_matrix[1, 1] / sum(confusion_matrix[1,:])
 
 def get_specificity(confusion_matrix):
-    return confusion_matrix[0, 0] / sum(confusion_matrix[0, :])
+    bottom = sum(confusion_matrix[0, :])
+    
+    return confusion_matrix[0, 0] / bottom
 
 
 def gini_index(data):
@@ -490,7 +495,7 @@ def cart_algorithm(data, features=None):
     if(is_single_class(data)):
         return classify(data)
     
-    
+    #print(features)
     partitions = get_data_partitions(data, features)
     question, value, gini = lowest_gini_split(data, partitions)
     
@@ -507,7 +512,7 @@ def cart_algorithm(data, features=None):
     
     return node
 
-def run_trials(num_trials, min_feature, max_feature, cost_function='Entropy'):
+def run_trials(num_trials, min_feature, max_feature, cost_function='Entropy', all_features=False):
     
     feature_to_sensitivity = defaultdict(list)
     feature_to_specificity = defaultdict(list)
@@ -526,11 +531,13 @@ def run_trials(num_trials, min_feature, max_feature, cost_function='Entropy'):
         train, test, data_train, data_test, child_to_index_train, child_to_index_test = get_train_test()
         
         for num_features in range(min_feature, max_feature + 1):
-
+            
+            features = None
             # get k most important features
-            features = select_k_lowest_features(data_train, k=num_features)
+            if(not all_features):
+                features = select_k_lowest_features(data_train, num_features, cost_function)
             #print(num_features)
-
+            #print(features)
             # build decision tree, get root back in tree
             tree = None
             if(cost_function == 'Entropy'):
@@ -653,5 +660,222 @@ def generate_performance_plots(feature_to_sensitivity, feature_to_specificity, f
     plt.title("Accuracy with {}".format(cost_function))
     plt.grid(color='lightgray')
     plt.show()
+    
+    
+def get_weighted_feature_to_counts(trees_dict):
+    feature_to_counts = defaultdict(int)
+    for feature in trees_dict:
+        trees = trees_dict[feature]
+        #print(feature)
+        for tree in trees:
+            # perform bfs and count feature occurance
+            
+            level = 0
+            q = Queue()
+            q.enqueue(tree)
+            while(not q.is_empty()):
+                size = q.size()
+                for x in range(size):
+                    cur_node = q.dequeue()
+                    question = cur_node.feature
+
+                    if(cur_node.no is not None):
+
+                        if(type(cur_node.no) != int):
+                            q.enqueue(cur_node.no)
+
+                    if(cur_node.yes is not None):
+
+                        if(type(cur_node.yes) != int):
+                            q.enqueue(cur_node.yes)
+
+                    feature_to_counts[question] += 1/(2**level) # weight count by 1/2^level
+
+                # next level
+                level += 1
+                
+        
+    
+    # normalize to probabilities
+    total_counts = sum(feature_to_counts.values())
+    feature_to_counts = {feature: count / total_counts for feature, count in feature_to_counts.items()}
+    return feature_to_counts
+
+def get_prob_vector(feature_counts):
+    probs = []
+    for x in range(30):
+        if(x in feature_counts.keys()):
+            probs.append(feature_counts[x])
+        else:
+            probs.append(0)
+    
+    return np.array(probs)
+
+def sorted_feature_table(features):
+    table = PrettyTable()
+    table.field_names = ["Feature", "Probability"]
+    for feature, probability in features:
+        table.add_row([feature, probability])
+    
+    return table
 
 
+def random_partition(data_partitions, probabilities=None):
+    
+    # get random feature
+    features, random_feature = None, None
+    if(probabilities is None):
+        features = list(data_partitions.keys())
+        random_feature = features[random.randint(0, len(features)-1)]
+    else:
+        features = np.arange(0,30)
+        random_feature = np.random.choice(30, 1, p=probabilities)[0]
+    
+    # get random value for that feature
+    values = data_partitions[random_feature]
+    random_value = values[random.randint(0, len(values)-1)]
+    
+    return random_feature, random_value
+
+def get_forest_classification(df, data, forest, child_to_index_test):
+    
+    actual_test = get_child_to_prediction(child_to_index_test, df)
+    actual = np.array(list(actual_test.values())).reshape(len(actual_test), 1)
+    
+    consensus_predictions = defaultdict(int)
+    preds = []
+    for tree in forest:
+        #print(tree)
+        pred_test = get_predictions(tree, data)
+        pred_test = child_to_prediction_validate(child_to_index_test, pred_test)
+        
+        pred = np.array(list(pred_test.values())).reshape(len(actual_test), 1)
+        
+        # get the confusion matrix
+        #c_matrix = confusion_matrix(pred, actual)
+        pred = np.squeeze(pred.reshape(1, len(pred)))
+        preds.append(pred)
+        
+    
+    for i in range(len(preds)):
+        for j in range(len(preds[i])):
+            consensus_predictions[j] += preds[i][j]
+    
+    final_predictions = []
+    for patient, votes in sorted(consensus_predictions.items(), key= lambda x: x[0]):
+        final_predictions.append(int(votes >= (len(forest) // 2)))
+    
+    final_predictions = np.array(final_predictions).reshape(len(final_predictions), 1)
+    
+    return final_predictions, actual
+
+def get_random_decision_tree(data, features=None, probabilities=None):
+    
+    if(is_single_class(data)):
+        return classify(data)
+    else:
+        # get question, and cutoff with lowest overall entropy
+        data_partitions = get_data_partitions(data, features)
+        split_question, split_value = random_partition(data_partitions)
+        
+        # no way to split the data with given features.. so return best guess
+        if(split_question == None):
+            #print("Model performing best guess for leaf node..")
+            return classify(data)
+            '''
+            for x in range(0, data.shape[1]-1):
+                if(x in features):
+                    print(data[:,x].shape)
+                    unique_scores = np.unique(data[:, x])
+                    print(x, data[:,x])
+                    print(unique_scores)
+            '''     
+        node = TreeNode(split_question, split_value)
+        a, b = partition_data(data, split_question, split_value)
+        
+        # recurse on left, and right subtrees..
+        node.yes = get_decision_tree(a, features)
+        node.no = get_decision_tree(b, features)
+        
+        return node
+
+
+def random_cart_algorithm(data, features=None, probabilities=None):
+    
+    if(is_single_class(data)):
+        return classify(data)
+    
+    #print(features)
+    data_partitions = get_data_partitions(data, features)
+    question, value = random_partition(data_partitions)
+    
+    # no way to further split the data
+    if(question is None):
+        return classify(data)
+    
+    a,b = partition_data(data, question, value)
+    
+    node = TreeNode(feature=question, value=value)
+    
+    node.yes = cart_algorithm(a, features)
+    node.no = cart_algorithm(b, features)
+    
+    return node
+
+def random_forest_trials(num_trees, num_trials, min_feature, max_feature, cost_function='Entropy', probabilities=None):
+    
+    feature_to_sensitivity = defaultdict(list)
+    feature_to_specificity = defaultdict(list)
+    feature_to_accuracy = defaultdict(list)
+    feature_to_uar = defaultdict(list)
+    
+    for num_features in range(min_feature, max_feature + 1):
+
+        for trial in range(num_trials):
+            
+            #print("Feature %d, Trial %d" % (num_features, trial+1))
+            train, test, data_train, data_test, child_to_index_train, child_to_index_test = get_train_test()
+
+            forest = []
+            for x in range(num_trees):
+                
+                features = None
+                if(probabilities is None):
+                    features = select_k_lowest_features(data_train, num_features, cost_function)
+                
+                if(cost_function == 'Entropy'):
+                    tree = get_random_decision_tree(data_train, features, probabilities)
+                    forest.append(tree)
+                elif(cost_function == 'Gini'):
+                    tree = random_cart_algorithm(data_train, features, probabilities)
+                    forest.append(tree)
+
+            voted_preds, actual = get_forest_classification(test, data_test, forest, child_to_index_test)
+            
+            #print(voted_preds, actual)
+            c_matrix = confusion_matrix(voted_preds, actual)
+            #print(c_matrix)
+            # get sensitivity, specificity, accuracy, and unweighted average recall
+            sensitivity = get_sensitivity(c_matrix)
+            
+            specificity = get_specificity(c_matrix)
+            #print(specificity)
+
+            accuracy = get_accuracy(c_matrix)
+
+            uar = (sensitivity + specificity) / 2
+            
+            feature_to_sensitivity[num_features].append(sensitivity)
+            feature_to_specificity[num_features].append(specificity)
+            feature_to_accuracy[num_features].append(accuracy)
+            feature_to_uar[num_features].append(uar)
+    
+    
+    return feature_to_sensitivity, feature_to_specificity, feature_to_accuracy, feature_to_uar
+
+
+def get_accuracy(confusion_matrix):
+    correct = sum(np.diagonal(confusion_matrix))
+    total = sum(sum(confusion_matrix))
+    
+    return correct / total
